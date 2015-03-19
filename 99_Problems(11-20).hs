943fb5c3125a470431b4e6fb346c0f971d2f24fd
khat33b <khat33b@gmail.com>
2015-03-19 06:31:11 +0530

    Till Problem 17

--- 99_Problems(11-20).hs
+++ 99_Problems(11-20).hs
@@ -11,8 +11,34 @@ encodeModified = map encodeHelper . encode
 		encodeHelper (1, x) = Single x
 		encodeHelper (n, x) = Multiple n x
 
-decodeModified :: [ListItem a] -> [a]
+decodeModified :: Eq a => [ListItem a] -> [a]
 decodeModified = concatMap decoder
 	where
 		decoder (Single x) = [x]
 		decoder (Multiple n x) = replicate n x
+
+encodeDirect :: (Eq a)=> [a] -> [ListItem a]
+encodeDirect [] = []
+encodeDirect (x:xs)
+    | count==1  = (Single x) : (encodeDirect xs)
+    | otherwise = (Multiple count x) : (encodeDirect rest)
+    where
+        (matched, rest) = span (==x) xs
+        count = 1 + (length matched)
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = 	x : x : dupli xs
+
+repli :: [a] -> Int -> [a]
+repli xs n = concatMap (replicate n) xs
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n = helper xs n
+    where helper [] _ = []
+          helper (x:xs) 1 = helper xs n
+          helper (x:xs) k = x : helper xs (k-1)
+
+split :: [a] -> Int -> ([a], [a])
+split (x:xs) n | n > 0 = let (f,l) = split xs (n-1) in (x : f, l)
+split xs _             = ([], xs)
