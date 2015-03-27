cfb8500b3099e7d7b108aa382cd615c942bad79f
Mohammed Khateeb Kamran <khat33b@gmail.com>
2015-03-27 20:15:34 +0530

    ordered into folders

--- 99_Problems(21-30).hs
+++
@@ -1,9 +0,0 @@
-import System.Random
-
-insertAt :: a -> [a] -> Int -> [a]
-insertAt y xs 1 = y : xs
-insertAt y (x:xs) n = x : insertAt y xs (n-1)
-
-range :: Int -> Int -> [Int]
-range m n | m == n = [n]
-range m n = m : range (m+1) n
